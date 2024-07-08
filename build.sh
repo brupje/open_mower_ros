538db9b702b0a7f9c830ab4d4688d06c0378f338
brupje <info@bnhmeijer.nl>
2024-07-08 21:12:54 +0200

    Sync fork

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #/bin/bash
-
-docker  build -t open_mower_ros:brupje . -f docker/Dockerfile
+git submodule update --init --recursive --remote 
+source /opt/prebuilt/slic3r_coverage_planner/setup.bash && catkin_make -DCATKIN_BLACKLIST_PACKAGES=slic3r_coverage_planner
