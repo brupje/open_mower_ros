301a07906ee4517860592947bda3cfc82e53086f
brupje <info@bnhmeijer.nl>
2025-05-31 12:03:38 +0200

    Restore

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #/bin/bash
 git submodule update --init --recursive --remote 
-source /opt/prebuilt/slic3r_coverage_planner/setup.bash && catkin_make -DCATKIN_BLACKLIST_PACKAGES=slic3r_coverage_planner
+catkin_make
