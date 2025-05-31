0367a042d6f2b87548b93294eb450dfd3a5693c7
brupje <info@bnhmeijer.nl>
2025-05-31 12:10:16 +0200

    Revert "Restore"
    This reverts commit 301a07906ee4517860592947bda3cfc82e53086f.

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #/bin/bash
 git submodule update --init --recursive --remote 
-catkin_make
+source /opt/prebuilt/slic3r_coverage_planner/setup.bash && catkin_make -DCATKIN_BLACKLIST_PACKAGES=slic3r_coverage_planner
