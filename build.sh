ee61e9e6d55f073cdf863dbcc994673580c1ecbd
brupje <info@bnhmeijer.nl>
2026-04-29 07:51:37 +0200

    naar brupje

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 #/bin/bash
 git submodule update --init --recursive --remote 
+
+rosdep install --from-paths src --ignore-src --default-yes
+
 source /opt/prebuilt/slic3r_coverage_planner/setup.bash && catkin_make -DCATKIN_BLACKLIST_PACKAGES=slic3r_coverage_planner
